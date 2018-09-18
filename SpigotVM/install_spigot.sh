1c53c99ac853fcfaa90269a430f4c141bccd360b
forqu <38428684+forqu@users.noreply.github.com>
2018-09-18 16:43:52 +0100

    fix install script

--- SpigotVM/install_spigot.sh
+++ SpigotVM/install_spigot.sh
@@ -4,4 +4,4 @@ sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubun
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo apt-get update -y
 sudo apt-get install docker-ce -y
-sudo docker run -e TYPE=PAPER -d -p 25565:25565 -e EULA=TRUE -e PAPER_DOWNLOAD_URL=goo.gl/7ENTX8 --name teamsmc itzg/minecraft-server --noconsole
+sudo docker run -e TYPE=PAPER -d -p 25565:25565 -e EULA=TRUE -e PAPER_DOWNLOAD_URL=https://yivesmirror.com/files/spigot/spigot-1.7.10-SNAPSHOT-b1657.jar --name teamsmc itzg/minecraft-server --noconsole
