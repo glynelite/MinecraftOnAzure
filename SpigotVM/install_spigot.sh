3e9c3baea274be360eeb24c20a5bb110702a83ca
forqu <38428684+forqu@users.noreply.github.com>
2018-09-19 12:26:01 +0100

    fix install script

--- SpigotVM/install_spigot.sh
+++ SpigotVM/install_spigot.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-echo "Hello world"
+
+docker pull itzg/minecraft-server
+sudo docker run -e TYPE=PAPER -d -p 25565:25565 -e EULA=TRUE -e PAPER_DOWNLOAD_URL=goo.gl/7ENTX8 --name draypus itzg/minecraft-server --noconsole
