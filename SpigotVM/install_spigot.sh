f9f7317f8840ccd3396d78756b8d562f889bf270
forqu <38428684+forqu@users.noreply.github.com>
2018-09-19 11:16:18 +0100

    fix install script

--- SpigotVM/install_spigot.sh
+++ SpigotVM/install_spigot.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-# Custom Spigot server install script for Ubuntu 18.4 lts VM
-
-docker pull itzg/minecraft-server
-docker run -d -p 25565:25565 --name mc itzg/minecraft-server
+echo Hello world !
