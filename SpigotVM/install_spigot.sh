b9489e7635c929fe0868905cd6c2dd5a92b3129c
glyn <glynsllis@gmail.com>
2018-10-02 01:12:35 +0100

    changed spigot container restart policy to always

--- SpigotVM/install_spigot.sh
+++ SpigotVM/install_spigot.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 docker pull itzg/minecraft-server
-sudo docker run -e TYPE=PAPER -d -p 25565:25565 -e EULA=TRUE -e PAPER_DOWNLOAD_URL=goo.gl/7ENTX8 --name draypus itzg/minecraft-server --noconsole
+sudo docker run --restart=always -e TYPE=PAPER -d -p 25565:25565 -e EULA=TRUE -e PAPER_DOWNLOAD_URL=goo.gl/7ENTX8 --name draypus itzg/minecraft-server --noconsole
