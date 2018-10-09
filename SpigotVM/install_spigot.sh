66152ca08b7a47d8b30782581050fa680f858171
glyn <glynsllis@gmail.com>
2018-10-09 22:59:42 +0100

    rollback install spigot script

--- SpigotVM/install_spigot.sh
+++ SpigotVM/install_spigot.sh
@@ -2,4 +2,4 @@
 
 apt-get install
 docker pull itzg/minecraft-server
-docker run --restart=always -e TYPE=PAPER -d -p 25565:25565 -e EULA=TRUE -e PAPER_DOWNLOAD_URL=goo.gl/7ENTX8 --name spigot itzg/minecraft-server --noconsole
+docker run --restart=always -e TYPE=PAPER -d -p 25565:25565 -e EULA=TRUE -e PAPER_DOWNLOAD_URL=goo.gl/3fuFgC --name spigot itzg/minecraft-server --noconsole
