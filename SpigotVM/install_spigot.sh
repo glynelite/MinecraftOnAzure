0c33cbe1dc84ab0b61629d3e22eb52aa2a769412
glyn <glynsllis@gmail.com>
2018-10-09 10:11:18 +0100

    add apt-get install to deploy script

--- SpigotVM/install_spigot.sh
+++ SpigotVM/install_spigot.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+apt-get install
 docker pull itzg/minecraft-server
-sudo docker run --restart=always -e TYPE=PAPER -d -p 25565:25565 -e EULA=TRUE -e PAPER_DOWNLOAD_URL=goo.gl/7ENTX8 --name spigot itzg/minecraft-server --noconsole
+docker run --restart=always -e TYPE=PAPER -d -p 25565:25565 -e EULA=TRUE -e PAPER_DOWNLOAD_URL=goo.gl/7ENTX8 --name spigot itzg/minecraft-server --noconsole
