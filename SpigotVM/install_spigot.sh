cf48956cbe2a67f4509366a4bbdad4c763104d32
glyn <glynsllis@gmail.com>
2018-10-15 00:35:43 +0100

    commit dockerfile and docker compose files

--- SpigotVM/install_spigot.sh
+++ SpigotVM/install_spigot.sh
@@ -2,4 +2,4 @@
 
 apt-get install
 docker pull itzg/minecraft-server
-docker run --restart=always -e TYPE=PAPER -d -p 25565:25565 -e EULA=TRUE -e PAPER_DOWNLOAD_URL=goo.gl/3fuFgC --name spigot itzg/minecraft-server --noconsole
+docker run --restart=always -e TYPE=PAPER -d -v /var/spigot-data:/data -p 25565:25565 -e EULA=TRUE -e PAPER_DOWNLOAD_URL=goo.gl/3fuFgC --name spigot itzg/minecraft-server --noconsole
