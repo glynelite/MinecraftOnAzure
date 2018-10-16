3bc4c950ffe327fd034f02816b052cbaa76b9a19
glyn <glynsllis@gmail.com>
2018-10-17 00:28:10 +0100

    automated creation of private key credentials

--- JenkinsVM/jenkins/init/credentials.groovy
+++ JenkinsVM/jenkins/init/credentials.groovy
@@ -1,14 +1,31 @@
 import com.cloudbees.plugins.credentials.impl.*;
+import com.cloudbees.jenkins.plugins.sshcredentials.impl.*;
 import com.cloudbees.plugins.credentials.*;
 import com.cloudbees.plugins.credentials.domains.*;
+
+def store = SystemCredentialsProvider.getInstance().getStore()
 
 def usernameGitHub = new File("/run/secrets/github-user").text.trim()
 def passwordGitHub = new File("/run/secrets/github-pass").text.trim()
 def credentialsIdGitHub = "github-credentials-id"
 def descriptionGitHub = "GitHub credentials"
+def privateKeyUser = "cragus"
+def privateKeyId = "spigkey"
 
-def store = SystemCredentialsProvider.getInstance().getStore()
+Credentials gitHubUser = (Credentials) new UsernamePasswordCredentialsImpl(
+	CredentialsScope.GLOBAL, 
+	credentialsIdGitHub, 
+	descriptionGitHub, 
+	usernameGitHub, 
+	passwordGitHub)
 
-Credentials gitHubUser = (Credentials) new UsernamePasswordCredentialsImpl(CredentialsScope.GLOBAL,credentialsIdGitHub, descriptionGitHub, usernameGitHub, passwordGitHub)
+Credentials privateKey = new BasicSSHUserPrivateKey(
+	CredentialsScope.GLOBAL,
+	privateKeyId, 
+	privateKeyUser,
+	new BasicSSHUserPrivateKey.FileOnMasterPrivateKeySource("/home/keyfile"),
+	"",
+	"remote host key")
 
 store.addCredentials(Domain.global(), gitHubUser)
+store.addCredentials(Domain.global(), privateKey)
